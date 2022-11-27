96e54c92e683798fb79edee73a9a7d30d1a7676b
osamakhanse <102169424+osamakhanse@users.noreply.github.com>
2022-11-27 22:52:43 +0000

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -6,7 +6,7 @@ resource "null_resource" "name" {
 
   connection {
     type        = "ssh"
-    user        = "ec2-user"
+    user        = "ubuntu"
     private_key = file("x21242887-jenkins.pem")
     host        = "ec2-34-242-102-85.eu-west-1.compute.amazonaws.com"
   }
