79e7828752bc19417cab14dbea6306305a5688a3
osamakhanse <102169424+osamakhanse@users.noreply.github.com>
2022-11-27 22:11:25 +0000

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -10,7 +10,7 @@ resource "aws_instance" "example" {
   connection {
     type        = "ssh"
     user        = "ec2-user"
-    private_key = file(pathexpand(".ssh/authorized_keys"))
+    private_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCpIOVaSMeAQOssBmGotprKxz8dUpcuLm9vq8F5vlWfQueIEwK21dUSHZDllKF/7lEacUzHIaUgdoM6ULrezUkRPN5TWvcXxAXvIDMNft9w6ZGeVI8w0qX7hOUWk6lHKFFMtijVspE9pYmNhApunG/oEMM0PPHSfLWLbCUruBglFputwdZSPtriw0a1AzoD2BbdvmQ5MIbIRAyEPjQ1mpA1L8EctgeOnuYhXOV/KNGZxLeBGhFpk8RVvkHRClJQqpkCXjFjDGgEcHW9kkfXughHGQ272MC889Li1KLIpm8DaKnekenTdEu1U1daX1iEOJOIkXsWcvDAzY8KtFfkD0q5 x21242887-jenkins"
     host        = self.public_ip
   }
 
