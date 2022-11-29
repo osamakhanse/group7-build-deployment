7884f9e1d986d76a0392429235e7ec0d8f53ffa7
osamakhanse <102169424+osamakhanse@users.noreply.github.com>
2022-11-29 16:24:45 +0000

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -8,12 +8,12 @@ resource "null_resource" "name" {
     type        = "ssh"
     user        = "ubuntu"
     private_key = file("x21242887-jenkins.pem")
-    host        = "ec2-34-242-102-85.eu-west-1.compute.amazonaws.com"
+    host        = "ec2-54-229-142-121.eu-west-1.compute.amazonaws.com"
   }
 
   provisioner "remote-exec" {
     inline = [
-      "sudo mkdir testhello"
+            "mkdir django-apps"
     ]
   }
 }
