d90bcb1240f49cbadec8f1f71be62a7a5f3a0f38
osamakhanse <102169424+osamakhanse@users.noreply.github.com>
2022-11-29 16:49:55 +0000

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -6,9 +6,9 @@ resource "null_resource" "name" {
 
   connection {
     type        = "ssh"
-    user        = "ec2"
+    user        = "ubuntu"
     private_key = file("x21242887-jenkins.pem")
-    host        = "1ec2-54-229-142-121.eu-west-1.compute.amazonaws.com"
+    host        = "ec2-54-229-142-121.eu-west-1.compute.amazonaws.com"
    
   }
   provisioner "remote-exec" {
