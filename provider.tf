437e3bc983cf75f6ae6ba34f4a0c3b7048d81e6e
osamakhanse <102169424+osamakhanse@users.noreply.github.com>
2022-11-29 16:39:02 +0000

    testingaws

--- provider.tf
+++ provider.tf
@@ -9,6 +9,7 @@ resource "null_resource" "name" {
     user        = "ubuntu"
     private_key = file("x21242887-jenkins.pem")
     host        = "ec2-54-229-142-121.eu-west-1.compute.amazonaws.com"
+   
   }
   provisioner "remote-exec" {
     inline = [
