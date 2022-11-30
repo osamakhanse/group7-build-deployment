c561a4903ed5bc1faeb5f4cb7ab8a3040a70f824
osamakhanse <102169424+osamakhanse@users.noreply.github.com>
2022-11-30 14:02:12 +0000

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,6 +2,20 @@ provider "aws" {
   region  = "eu-west-1"
 }
 resource "null_resource" "name" {
+  
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = file("x21242887-jenkins.pem")
+    host        = "ec2-34-244-92-217.eu-west-1.compute.amazonaws.com"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "suro rm -r AOE"
+    ]
+  }
+  
   provisioner "file" {
     source      = "AOE_django-main"
     destination = "AOE"
@@ -16,17 +30,11 @@ resource "null_resource" "name" {
     
   }
   
-  connection {
-    type        = "ssh"
-    user        = "ubuntu"
-    private_key = file("x21242887-jenkins.pem")
-    host        = "ec2-34-244-92-217.eu-west-1.compute.amazonaws.com"
-  }
-
-  provisioner "remote-exec" {
+   provisioner "remote-exec" {
     inline = [
-      "sudo mkdir appsteam7-4"
+      "sudo mkdir appsteam7-5"
     ]
   }
+  
 }
 
