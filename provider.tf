b9e4f6bf5f402094b3e9fc10990b466694b9c915
osamakhanse <102169424+osamakhanse@users.noreply.github.com>
2022-11-30 10:55:29 +0000

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,18 +1,18 @@
 provider "aws" {
   region  = "eu-west-1"
 }
+resource "null_resource" "name" {
+  provisioner "file" {
+    source      = "/AOE_django-main"
+    destination = "/AOE"
 
-provisioner "file" {
-  source      = "/AOE_django-main"
-  destination = "/AOE"
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file("x21242887-jenkins.pem")
+      host        = "ec2-34-244-92-217.eu-west-1.compute.amazonaws.com"
 
-  connection {
-    type        = "ssh"
-    user        = "ubuntu"
-    private_key = file("x21242887-jenkins.pem")
-    host        = "ec2-34-244-92-217.eu-west-1.compute.amazonaws.com"
-   
+    }
   }
 }
-
 
